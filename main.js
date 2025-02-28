6109a06da92d37838d6dc46fa3105bf14ea3f453
Yubeen Om <yubeen.om@gmail.com>
2025-03-01 02:51:51 +0900

    fourth commit

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let newsList = []
+const menus = document.querySelectorAll(".menus button")
+menus.forEach(menu => menu.addEventListener("click", (event)=> getNewsByCategory(event)))
 
 const getLatestNews = async () => {
     const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`)
@@ -12,6 +14,27 @@ const getLatestNews = async () => {
 
 getLatestNews();
 
+const getNewsByCategory = async (event)=>{
+  const category = event.target.textContent.toLowerCase()
+  console.log("category ", category)
+  const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${category}`)
+  const response = await fetch(url)
+  const data =await response.json()
+  console.log("ddd",data)
+  newsList = data.articles
+  render()
+}
+
+const searchNews = async () => {
+  const keyword = document.getElementById("search-input").value
+  console.log("keyword", keyword)
+  const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&q=${keyword}`)
+  const response = await fetch(url)
+  const data = await response.json()
+  console.log("keyword data",data)
+  newsList = data.articles
+  render()
+}
 
 const render=()=>{
     const newsHTML = newsList.map(news=>`<div class="row news">
@@ -55,3 +78,7 @@ const openNav = () => {
     }
   };
   
+
+  // 1. 버튼들에 클릭 이벤트를 줘야함
+  // 2. 카테고리별 뉴스 가져오기
+  // 3. 그 뉴스를 보여주기 render
